8e8c5d6cc409ca6bfa4e38f70567b4fc0bf23391
Eiki Martinson <eiki@eikimartinson.com>
2011-05-20 21:05:41 -0400

    Improved documentation, made the script output the friction factor when run at the command line.

--- colebrook.m
+++ colebrook.m
@@ -10,8 +10,10 @@ f_initial = 0.035; %Initial value guessed at by aiming at the
 Re = 25300;
 epsilon = 0.002;
 d = 25.4;          %Last two values both in millimeters
+
 r = epsilon / d;   %Roughness ratio
 f = f_initial;
 for i = 1:10
   f = (2.0 * log10((r/3.7) + (2.51/(Re*f^0.5))))^-2;
 end
+f
